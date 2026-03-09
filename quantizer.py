bf694757a4cfd03fb9bc315425b83b04ede7e7e2
Jyun Jeon <hedgehog412@gmail.com>
2026-03-09 14:29:29 -0400

    Added vqvae

---
+++ quantizer.py
@@ -0,0 +1,93 @@
+import torch
+import torch.nn as nn
+
+class VectorQuantizer(nn.Module):
+  def __init__(self, num_embeddings = 512, embedding_dim = 64, commitment_cost = 0.25):
+    super().__init__()
+
+    self.embedding_dim = embedding_dim
+    self.num_embeddings = num_embeddings
+    self.commitment_cost = commitment_cost
+
+    self.embedding = nn.Embedding(num_embeddings, embedding_dim)
+    self.embedding.weight.data.uniform_(-1.0 / num_embeddings, 1.0 / num_embeddings)
+
+  def forward(self, z_e):
+    z = z_e.permute(0, 2, 3, 1).contiguous()
+    z_flattened = z.view(-1, self.embedding_dim)
+
+    z_sq = torch.sum(z_flattened ** 2, dim = 1, keepdim=True)
+    e_sq = torch.sum(self.embedding.weight ** 2, dim = 1)
+    ze = torch.matmul(z_flattened, self.embedding.weight.t())
+    d = z_sq + e_sq.unsqueeze(0) - 2 * ze
+
+    encoding_indices = torch.argmin(d, dim = 1)
+
+    quantized = self.embedding(encoding_indices).view(z.shape)
+
+    codebook_loss = F.mse_loss(quantized, z.detach())
+    commitment_loss = F.mse_loss(z, quantized.detach())
+    vq_loss = codebook_loss + self.commitment_cost * commitment_loss
+
+    quantized_st = z + (quantized - z).detach()
+
+    encodings = F.one_hot(encoding_indices, self.num_embeddings).float()
+    avg_probs = encodings.mean(0)
+    perplexity = torch.exp(-torch.sum(avg_probs * torch.log(avg_probs + 1e-10)))
+
+    z_q = quantized_st.permute(0, 3, 1, 2).contiguous()
+    indices = encoding_indices.view(z.shape[0], z.shape[1], z.shape[2])
+
+    return z_q, vq_loss, perplexity, indices
+
+class VectorQuantizerEMA(nn.Module):
+  def __init__(self, decay = 0.99, epsilon = 1e-5, num_embeddings = 512, embedding_dim = 64, commitment_cost = 0.25):
+    super().__init__()
+    self.embedding_dim = embedding_dim
+    self.num_embeddings = num_embeddings
+    self.commitment_cost = commitment_cost
+
+    self.embedding = nn.Embedding(num_embeddings, embedding_dim)
+    self.embedding.weight.data.uniform_(-1.0 / num_embeddings, 1.0 / num_embeddings)
+
+    self.register_buffer("ema_cluster_size", torch.zeros(num_embeddings))
+    self.register_buffer("ema_w", self.embedding.weight.data.clone())
+
+    self.decay = decay
+    self.epsilon = epsilon
+
+  def forward(self, z_e):
+    z = z_e.permute(0, 2, 3, 1).contiguous()
+    z_flattened = z.view(-1, self.embedding_dim)
+
+    z_sq = torch.sum(z_flattened ** 2, dim = 1, keepdim=True)
+    e_sq = torch.sum(self.embedding.weight ** 2, dim = 1)
+    ze = torch.matmul(z_flattened, self.embedding.weight.t())
+    d = z_sq + e_sq.unsqueeze(0) - 2 * ze
+
+    encoding_indices = torch.argmin(d, dim = 1)
+    quantized = self.embedding(encoding_indices).view(z.shape)
+
+    if self.training:
+      enc_onehot = F.one_hot(encoding_indices, self.num_embeddings).type_as(z_flattened)
+      self.ema_cluster_size = self.decay * self.ema_cluster_size + (1 - self.decay) * torch.sum(enc_onehot, 0)
+      self.ema_w = self.decay * self.ema_w + (1 - self.decay) * torch.matmul(enc_onehot.t(), z_flattened)
+
+      n = torch.sum(self.ema_cluster_size.data)
+      cluster_size_smoothed = (self.ema_cluster_size + self.epsilon) / (n + self.num_embeddings * self.epsilon) * n
+      new_weight = self.ema_w / cluster_size_smoothed.unsqueeze(1)
+      self.embedding.weight.data.copy_(new_weight)
+
+    commitment_loss = F.mse_loss(z, quantized.detach())
+    vq_loss =  self.commitment_cost * commitment_loss
+
+    quantized_st = z + (quantized - z).detach()
+
+    encodings = F.one_hot(encoding_indices, self.num_embeddings).float()
+    avg_probs = encodings.mean(0)
+    perplexity = torch.exp(-torch.sum(avg_probs * torch.log(avg_probs + 1e-10)))
+
+    z_q = quantized_st.permute(0, 3, 1, 2).contiguous()
+    indices = encoding_indices.view(z.shape[0], z.shape[1], z.shape[2])
+
+    return z_q, vq_loss, perplexity, indices
---
+++ vqvae.py
@@ -0,0 +1,83 @@
+import torch
+import torch.nn as nn
+from quantizer import VectorQuantizer, VectorQuantizerEMA
+
+class ConvBlock(nn.Module):
+  def __init__(self, in_ch, out_ch, k = 3, s = 1, p = 1, use_gn = True, gn_groups = 8):
+    super().__init__()
+
+    self.conv = nn.Conv2d(in_ch, out_ch, k, stride = s, padding = p)
+
+    self.norm = None
+    if use_gn:
+      g = min(gn_groups, out_ch)
+      while out_ch % g != 0 and g > 1:
+        g -= 1
+      self.norm = nn.GroupNorm(g, out_ch)
+    else:
+      self.norm = nn.BatchNorm2d(out_ch)
+
+    self.act = nn.ReLU(inplace=True)
+
+  def forward(self, x):
+    x = self.conv(x)
+    if self.norm is not None:
+      x = self.norm(x)
+    return self.act(x)
+
+class Encoder(nn.Module):
+  def __init__(self, in_channels=1, hidden=128, z_channels = 64):
+    super().__init__()
+
+    self.net = nn.Sequential(
+        ConvBlock(in_channels, hidden // 2, k = 4, s = 2, p = 1),
+        ConvBlock(hidden // 2, hidden, k = 4, s = 2, p = 1),
+        ConvBlock(hidden, hidden, k = 4, s = 2, p = 1),
+        ConvBlock(hidden, hidden, k = 3, s = 1, p = 1),
+        nn.Conv2d(hidden, z_channels, kernel_size=1)
+    )
+  def forward(self, x):
+    return self.net(x)
+
+class Decoder(nn.Module):
+  def __init__(self, out_channels = 1, hidden = 128, z_channels = 64):
+    super().__init__()
+
+    self.net = nn.Sequential(
+        ConvBlock(z_channels, hidden, k = 3, s = 1, p = 1),
+        ConvBlock(hidden, hidden, k = 3, s = 1, p = 1),
+
+        nn.ConvTranspose2d(hidden, hidden, kernel_size=4, stride = 2, padding=1),
+        nn.ReLU(inplace=True),
+
+        nn.ConvTranspose2d(hidden, hidden, kernel_size=4, stride = 2, padding=1),
+        nn.ReLU(inplace=True),
+
+        nn.ConvTranspose2d(hidden, hidden//2, kernel_size=4, stride = 2, padding=1),
+        nn.ReLU(inplace=True),
+
+        nn.Conv2d(hidden // 2, out_channels, 1)
+    )
+  def forward(self, x):
+    return self.net(x)
+
+class VQVAE(nn.Module):
+  def __init__(self, in_channels = 1, hidden = 128, z_channels = 64, num_embeddings=512, commitment_cost=0.25, decay=0.995, epsilon=1e-5, num_groups=8):
+    super().__init__()
+
+    self.encoder = Encoder(in_channels=in_channels, hidden=hidden, z_channels=z_channels)
+    self.pre_vq_norm = nn.GroupNorm(num_groups=num_groups, num_channels=z_channels)
+
+    # self.vq = VectorQuantizer(num_embeddings=num_embeddings, embedding_dim=z_channels, commitment_cost=commitment_cost)
+    self.vq = VectorQuantizerEMA(decay=decay, epsilon=epsilon, num_embeddings=num_embeddings, embedding_dim=z_channels, commitment_cost=commitment_cost)
+
+    self.decoder = Decoder(out_channels=in_channels, hidden=hidden, z_channels=z_channels)
+
+  def forward(self, x):
+    z_e = self.encoder(x)
+    z_e = self.pre_vq_norm(z_e)
+    z_q, vq_loss, perplexity, indices = self.vq(z_e)
+    x_recon = self.decoder(z_q)
+
+    return x_recon, vq_loss, perplexity, indices
+
